5c5d95d4008ea5654cffe12e06aaad1bb12fdd4d
Ece Ozalp <github email address>
2016-07-28 13:38:53 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    new_array = []
+    array.each do |x|
+      if x.chars.sort == @word.chars.sort
+        new_array << x
+      end
+    end
+    new_array
+  end
+end
+
